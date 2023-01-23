6f966ec68a61c700bab66f519ad5de124d12e0cd
Henry Schreiner <henryschreineriii@gmail.com>
2023-01-23 14:32:54 -0500

    feat: from cookiecutter
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ src/sqat_example/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 Henry Schreiner. All rights reserved.
+
+sqat-example: A great package.
+"""
+
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
+
+__all__ = ("__version__",)
---
+++ tests/test_package.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import sqat_example as m
+
+
+def test_version():
+    assert m.__version__
